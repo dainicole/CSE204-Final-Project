f27a18def466014e76bf7ee628e8363bf1a2e8d1
dainicole <143472791+dainicole@users.noreply.github.com>
2025-12-26 10:17:06 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/final-project-dainicole/',
+  base: "/CSE204-Final-Project/",
   plugins: [react()],
 })
